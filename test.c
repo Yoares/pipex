425bde383b125ecdf99296a55ea54e4594f84caf
Yasser Khoussi <ykhoussi@e1r4p9.1337.ma>
2025-01-25 17:01:56 +0100

    7alawa

--- test.c
+++ test.c
@@ -8,37 +8,45 @@ int main(int ac, char *av[])
 {
     int n;
 
-    int fd = open("fd.txt", O_CREAT | O_RDWR | O_TRUNC, 0644);
-    int fd1 = open("fd1.txt", O_CREAT | O_RDWR | O_TRUNC, 0644);
+    int pipefd[2];
 
-    if (fd == -1 || fd1 == -1)
+    if (pipe(pipefd) == -1)
     {
-        perror("error");
+        perror ("pipeerror");
         return 1;
     }
 
     int id = fork();
+    if (id == -1)
+    {
+        perror ("errorfork");
+        return 1;
+    }
+
     if (id == 0)  // child process l3ar
     {
+        close(pipefd[0]); // Close lkhra
         n = 1;
         int i = n;
         while (i < n + 5)
         {
             char c = i + '0';
-            write(fd, &c , 1);
+            write(pipefd[1], &c , 1);
             i++;      
         }
+        close(pipefd[1]);
     }
     else  {
-        n = 5;
+        close(pipefd[1]); // Close type shit
         wait(NULL);
-        int i = n;
-        while (i < n + 5)
+
+        char buffer[1];
+        while (read(pipefd[0], buffer, sizeof(char)) > 0)
         {
-            char c = i + '0';
-            write(fd1, &c, 1);
-            i++;      
-        }     
+            write(STDOUT_FILENO, buffer, 1);
+        }
+        close(pipefd[0]);
+        printf("\n");
     }
-    printf("\n");
+
 }
